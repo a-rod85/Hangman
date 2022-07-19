a37f56d45865b45256147b613398f47e26b7e698
a-rod85 <31aaronanderson@gmail.com>
2022-07-19 11:13:08 +0000

    add import random from words import words add def get_valid_word word will = a random selceted word in the game add name input and message which will print

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from words import words
+
+#will generate valid random words
+
+def get_valid_word(words):
+    word = random.choice(words)
+    while "-" in word or " " in word: 
+        word = random.choice(words)
+
+        return word.upper
+
+        name = input ("What's your name?")
+        print(f"All the best"{name}!)
