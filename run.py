0a4cc43907c33c46f67375a9219e2e39372ed14b
a-rod85 <31aaronanderson@gmail.com>
2022-07-19 16:13:50 +0000

    add def for name, user can now enter their name add while,if & else function for def name

--- run.py
+++ run.py
@@ -1,13 +1,35 @@
 import random
 from words import words
 
-#will generate valid random words
+print(
+    """
+    Welcome to Hangman the  game!
+    The rules are simple, you are given a random word,
+    then you have 6 turns to guess which word
+    is lurking in the wilderness!
+    """
+    )
 
 def get_valid_word(words):
     word = random.choice(words)
     while "-" in word or " " in word: 
         word = random.choice(words)
 
-        return word.upper
+        return word.upper()
+
+def name():
+    """
+    Askes user to enter his/her name
+    """
+    while True:
+        name = input("What's your name?\n")
+
+    if name.isalpha():
+        print(f"Best of luck {name}!")
+        run_game()
+        break
+       
+    else:
+            print("Not a valid name, please try again")
 
        
